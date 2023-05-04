6d4ec81dcd74d7f48b9b356c724dcc87cfda7544
rahiyansafin <rahiyan@binaryquest.com>
2023-05-04 07:47:06 +0600

    Infrastructure layer added

---
+++ HR.LeaveManagement.Persistence/InfrastructureServicesRegistration.cs
@@ -0,0 +1,20 @@
+﻿using HR.LeaveManagement.Application.Contracts.Email;
+using HR.LeaveManagement.Application.Contracts.Logging;
+using HR.LeaveManagement.Application.Models.Email;
+using HR.LeaveManagement.Persistence.EmailService;
+using HR.LeaveManagement.Persistence.Logging;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HR.LeaveManagement.Persistence;
+public static class InfrastructureServicesRegistration
+{
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.Configure<EmailSettings>(configuration.GetSection("EmailSettings"));
+        services.AddTransient<IEmailSender, EmailSender>();
+        services.AddScoped(typeof(IAppLogger<>), typeof(LoggerAdapter<>));
+        return services;
+    }
+}
